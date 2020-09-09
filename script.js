59b49420e5b76d16c904e1f63eb7ca37cffe7640
Łukasz Terpiłowski <lukaszterpilowski9@gmail.com>
2020-09-09 19:25:21 +0200

    complete drawing advice

--- script.js
+++ script.js
@@ -6,7 +6,16 @@ const returnBtn = document.querySelector('.return_btn');
 //Sections
 const mainSection = document.querySelector('.main_section');
 const favsSadivesSection = document.querySelector('.favs_adives_section');
+const result = document.querySelector('.result');
 
+const favouriteAdvices = [];
+
+class Adice {
+    constructor(adviceText, id){
+        this.adviceText = adviceText;
+        this.id = id;
+    }
+}
 
 // Interface
 
@@ -17,9 +26,43 @@ function swapFavMainSections() {
     favsSadivesSection.classList.toggle('unactive');
 }
 
-function renderAdvice() {
+function renderDrawnAdvice(newAdvice, place) {
+    const prevAdvice = place.querySelector('.advice');
+    if(place == result && prevAdvice != null)
+        place.removeChild(prevAdvice);
     
+
+    const advice = document.createElement('div');
+    advice.classList.add('advice');
+    advice.dataset.id = newAdvice.id;
+    
+    const adviceText = document.createElement('p');
+    adviceText.textContent = newAdvice.adviceText;
+    advice.appendChild(adviceText);
+
+    //const favouriteIcon;
+
+    place.appendChild(advice);
 }
 
 showFavouritesBtn.addEventListener('click', swapFavMainSections);
 returnBtn.addEventListener('click', swapFavMainSections);
+
+// Using API
+
+
+function getAdvice() {
+    const url = 'https://api.adviceslip.com/advice';
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const newAdvice = new Adice(data.slip.advice, data.slip.id);
+            renderDrawnAdvice(newAdvice, result);
+
+        });
+}
+
+drawAdviceBtn.addEventListener('click', () => {
+    getAdvice();
+
+});
